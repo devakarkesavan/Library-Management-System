8e09b59d488089f12732190349521872704db27e
Devakar <devakarkesavan@gmail.com>
2024-02-21 09:33:19 +0530

    second commit

--- server/db.js
+++ server/db.js
@@ -1,11 +1,8 @@
-const Pool = require("pg").Pool
+const { Pool } = require("pg");
 
+// Update the connection string with your Neon PostgreSQL credentials
 const pool = new Pool({
-    user: "postgres",
-    password:"De34421227@",
-    host:"localhost",
-    port: 5432,
-    database: "perntodo"
-})
+    connectionString: 'postgresql://devakarkesavan:Cyj0XpbW4rHM@ep-royal-cherry-a1pd1vdv.ap-southeast-1.aws.neon.tech/Lib-manage?sslmode=require'
+});
 
 module.exports = pool;
