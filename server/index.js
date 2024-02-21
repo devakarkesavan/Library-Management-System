2e12007904836541f4e1eec9aca5b5d126860892
Devakar <devakarkesavan@gmail.com>
2024-02-21 12:47:42 +0530

    nineth commit

--- server/index.js
+++ server/index.js
@@ -77,7 +77,7 @@ app.delete("/Book/:id", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5002;
+const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started running on port ${PORT}`);
